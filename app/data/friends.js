ea61d5a58a181b1b00b8fce9d37e6c89f5bf1ecc
syin <shaomingyinut@gmail.com>
2018-09-08 21:51:59 -0400

    populating friends starter data

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+// array of friends
+var friendsarray = [
+  {
+    name: 'Tom Cruise',
+    photo:
+      'https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w700.h700.jpg',
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: 'Brad Pitt',
+    photo:
+      'http://specials-images.forbesimg.com/imageserve/5776a952a7ea436bd18c1204/416x416.jpg?background=000000&cropX1=150&cropX2=543&cropY1=24&cropY2=417',
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: 'Jennifer Lawrence',
+    photo:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Jennifer_Lawrence_SDCC_2015_X-Men.jpg/220px-Jennifer_Lawrence_SDCC_2015_X-Men.jpg',
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: 'Anne Hathaway',
+    photo:
+      'https://s1.r29static.com//bin/entry/ee3/340x408,80/1936787/image.jpg',
+    scores: [3, 4, 3, 4, 3, 4, 3, 4, 3, 4]
+  },
+  {
+    name: 'Lebron James',
+    photo:
+      'https://thefader-res.cloudinary.com/private_images/w_760,c_limit,f_auto,q_auto:best/GettyImages-968727464_u6ymkm/donald-trump-insults-lebron-james-twitter.jpg',
+    scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
+  },
+  {
+    name: 'Roger Federer',
+    photo:
+      'https://sportshub.cbsistatic.com/i/r/2017/08/31/04976f79-b1d8-4274-a326-44425a966eae/resize/670x377/929a2a1562ce47a5a91c2e5ea779c3c9/roger-federer.jpg',
+    scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  },
+  {
+    name: 'Serena Williams',
+    photo:
+      'https://assets1.sportsnet.ca/wp-content/uploads/2018/08/Serena-Williams-1040x572.jpg',
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: 'Usain Bolt',
+    photo:
+      'https://i2.wp.com/www.thesun.co.uk/wp-content/uploads/2018/02/nintchdbpict000345455626.jpg?crop=20px%2C43px%2C2203px%2C1467px&resize=1200%2C800&ssl=1',
+    scores: [1, 5, 1, 5, 1, 5, 1, 5, 1, 5]
+  }
+];
+
+module.exports = friendsarray;
