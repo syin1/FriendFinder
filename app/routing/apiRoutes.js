e6e329e3ca72e1ceac8d942a8916098399367c0d
syin <shaomingyinut@gmail.com>
2018-09-09 14:46:31 -0400

    implemented handling method for survey post request, and a simple score comparison algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,25 @@ module.exports = function(app) {
   });
 
   app.post('/api/friends', function(req, res) {
-    console.log(req.body);
+    // initialization
+    var lowestscore = 100; // cannot be higher or equal to this number
+    name = '';
+    photo = '';
+
+    for (var i = 0; i < friends.length; i++) {
+      var individualscore = 0;
+      for (var j = 0; j < req.body.scores.length; j++) {
+        individualscore += Math.abs(friends[i].scores[j] - req.body.scores[j]);
+      }
+
+      if (individualscore < lowestscore) {
+        name = friends[i].name;
+        photo = friends[i].photo;
+        lowestscore = individualscore;
+      }
+    }
+
+    friends.push(req.body);
+    res.json({ status: 'OK', name: name, photo: photo });
   });
 };
