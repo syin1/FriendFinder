ca8f67f4e668c460a21f8baf99c5a3fa52cee5f7
syin <shaomingyinut@gmail.com>
2018-09-09 09:19:07 -0400

    update survey post link

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,8 @@ module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
     res.json(friends);
   });
+
+  app.post('/api/friends', function(req, res) {
+    console.log(req.body);
+  });
 };
